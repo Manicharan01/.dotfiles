c0e19f321f61b89f6e6677acbbe9700201d94b7a
Manicharan <manicharan150@gmail.com>
2024-09-05 00:46:27 +0530

    Nothing

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font({ family = "MesloLGS NF", weight = "Regular", style = "Normal" })
+config.font = wezterm.font({ family = "MesloLGS Nerd Font Mono", weight = "Regular", style = "Normal" })
 config.color_scheme = "rose-pine"
 config.term = "xterm-256color"
 config.force_reverse_video_cursor = true
