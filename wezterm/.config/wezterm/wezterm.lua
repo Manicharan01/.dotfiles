c22b326b577ff002e08302a712429da485830005
Manicharan01 <manicharan@gmail.com>
2024-09-19 23:36:31 +0530

    Something

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("MesloLGS Nerd Font")
-config.color_scheme = "rose-pine"
+config.font = wezterm.font("MesloLGS NF")
+config.color_scheme = "Catppuccin Mocha"
 config.term = "xterm-256color"
 config.force_reverse_video_cursor = true
 config.enable_tab_bar = false
