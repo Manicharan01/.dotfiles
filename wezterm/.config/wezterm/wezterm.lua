2d23ba163bc8eb5822bac18f1e82a5fc0ff16e0a
Manicharan <manicharan150@gmail.com>
2024-11-26 12:10:17 +0530

    Made changes to the wallpapers folder and wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,18 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("MesloLGS Nerd Font")
-config.color_scheme = "Catppuccin Mocha"
-config.term = "screen-256color"
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.term = "alacritty"
 config.force_reverse_video_cursor = true
 config.enable_tab_bar = false
+config.window_padding = {
+    top = 8,
+    right = 8,
+    bottom = 8,
+    left = 8,
+}
+config.enable_wayland = true
+config.color_scheme = 'Default Dark (base16)'
+config.bold_brightens_ansi_colors = true
 
 return config
